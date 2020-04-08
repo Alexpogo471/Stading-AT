57772264ac165ce6e3cfc164d2a2e00e7e5a62d1
Alexpogo471 <56255801+Alexpogo471@users.noreply.github.com>
2020-04-08 22:50:14 +0300

    add class Vector2d

--- src/Vector2d.java
+++ src/Vector2d.java
@@ -1,2 +1,46 @@
 public class Vector2d {
+
+    private double x, y;
+
+    public Vector2d(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector2d{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    public Vector2d add(Vector2d a) {
+        return new Vector2d(this.x + a.getX(),
+                this.y + a.getY());
+    }
+
+    public Vector2d subtract(Vector2d a) {
+        return new Vector2d(this.x - a.getX(),
+                this.y - a.getY());
+    }
+
+    public Vector2d scalarMultiply(Vector2d a) {
+        return new Vector2d(this.x * a.getX(),
+                this.y * a.getY());
+    }
+
+    public Vector2d vectorMultiply(Vector2d a) {
+
+        return new Vector2d(this.getX() * a.getX(),
+                this.getY() * a.getY());
+    }
 }
