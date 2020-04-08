5e7ea97d76cdc4abac0de193e74129c1b4c0c6fe
Alexpogo471 <56255801+Alexpogo471@users.noreply.github.com>
2020-04-08 22:50:55 +0300

    add class Main, and check project - worked

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,16 @@
 public class Main {
 
+    public static void main(String[] args) {
+
+        Vector2d vector2d = new Vector2d(2,6);
+        Vector2d vector2d1 = new Vector2d(3,5);
+        Vector3d vector3d = new Vector3d(4,6,2);
+        Vector3d vector3d1 = new Vector3d(3,9,7);
+
+        System.out.println(vector2d.add(vector2d1));
+        System.out.println(vector3d.subtract(vector3d1));
+        System.out.println(vector3d.scalarMultiply(vector3d1));
+        System.out.println(vector3d.vectorMultiply(vector3d1));
+
+    }
 }
