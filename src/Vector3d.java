3b2cb7ae3e8acdd70f2c2b4a1708f1f051bf85d4
Alexpogo471 <56255801+Alexpogo471@users.noreply.github.com>
2020-04-08 22:49:46 +0300

    add class Vector3d

--- src/Vector3d.java
+++ src/Vector3d.java
@@ -1,3 +1,60 @@
 public class Vector3d {
 
+    private double x, y, z;
+
+    public Vector3d(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector3d{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public Vector3d add(Vector3d a) {
+        return new Vector3d(this.x + a.getX(),
+                this.y + a.getY(),
+                this.z + a.getZ());
+    }
+
+    public Vector3d subtract(Vector3d a) {
+        return new Vector3d(this.x - a.getX(),
+                this.y - a.getY(),
+                this.z - a.getZ());
+    }
+
+    public Vector3d scalarMultiply(Vector3d a) {
+        return new Vector3d(this.x * a.getX(),
+                this.y * a.getY(),
+                this.z * a.getZ());
+    }
+
+    public Vector3d vectorMultiply(Vector3d a) {
+        double newX, newY, newZ;
+
+        newX = this.getY() * a.getZ() - this.getZ() * a.getY();
+        newY = this.getZ() * a.getX() - this.getX() * a.getZ();
+        newZ = this.getX() * a.getY() - this.getY() * a.getX();
+
+        return new Vector3d(newX, newY, newZ);
+    }
 }
+
